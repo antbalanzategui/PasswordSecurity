9df169b6a28dfae13f9e79b77c6c113e62c65fea
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-12-02 18:15:47 -0500

    Finalized Algo

--- src/HW13/PasswordSec.py
+++ src/HW13/PasswordSec.py
@@ -5,12 +5,22 @@ Created on Nov 28, 2022
 '''
 import hashlib 
 from time import time
+from collections import deque
+import sys
+import matplotlib
+
+
+
+sys.setrecursionlimit(5000)
 
 tFile = open("common-passwords-win.txt")
 contents = tFile.readlines()
 i = 0
 listOfWords = ""
 d = {}
+wordList = []
+inputList = []
+numberOfGuess = []
 # While loop to create a string of all words within
 # the read text file
 while i < len(contents):
@@ -19,6 +29,7 @@ while i < len(contents):
     concat += str(i+1)
     concat += ": "
     concat += contents[i]
+    wordList.append(contents[i].replace('\n', ""))
     if (i != len(contents) - 1):
         concat += ", "
     listOfWords += concat
@@ -31,23 +42,49 @@ result = dict((b.strip(), int(a.strip()))
     for a, b in (element.split(':')
         for element in listOfWords.split(', '))) 
 
+# Dictionary Swap
+result_swap = {v: k for k, v in result.items()}
 #Array Objects which will hold times 
 timeArray256 = []
 timeArray512 = []
+
+def recExtras(l, theSum):
+    if (l == 3):
+        return theSum + 12
+    else:
+        theSum = theSum + l * 2
+        return recExtras(l - 1, theSum)
+
+def emptyQueue(lister):
+    i = 0 
+    queue = deque()
+    while i < len(lister):
+        queue.append("")
+        i = i + 1
+    return queue
+
+
+queue = emptyQueue(wordList)
+extra = recExtras(len(wordList), 0)
  
 # Void method to requestInput from a user, responsible for 
 # entire program, encapsulates all other methods
 def requestInput():
     userInput = input("Please Enter a Password to Crack: ")
-    if (validateInput(userInput, result) == True):
+    if (recValidateInput(userInput, result, 0, 0, 0, "") == True):
+        
         print("SHA 256: "+hashInput_256(userInput))
         print("SHA 512: "+hashInput_512(userInput))
-        time = passwordCrack_256(result, userInput)
-        time2 = passwordCrack_512(result, userInput)
-        timeArray256.append(time)
-        timeArray512.append(time2)
-        print("Time for SHA256: ", time)
-        print("Time for SHA512: ", time2)
+        inputList.append(userInput)
+        start = time()
+        numberOfGuess.append(recCombos512(wordList, emptyQueue(wordList), 0, 3, 0, 0, userInput, extra))
+        newTime = time() - start
+        timeArray512.append(newTime)
+        recCombos512(wordList, emptyQueue(wordList), 0, 3, 0, 0, userInput, extra)
+        newTime2 = time() - start
+        timeArray256.append(newTime2)
+        print("Time for SHA256: ", newTime)
+        print("Time for SHA512: ", newTime2)
         return userInput
     elif (userInput == 'q'):
         print("Exitted Console")
@@ -55,22 +92,37 @@ def requestInput():
         print("Invalid Input!")
     return userInput
 # Method to ensure a user input is valid 
-def validateInput(uInput, d):
-    inputLength = len(uInput)
-    i = 0
-    counter = 0
-    limitCounter = 0
-    validationString = ""
-    while (i < inputLength):
+#def validateInput(uInput, d):
+    #inputLength = len(uInput)
+    #i = 0
+    #counter = 0
+    #limitCounter = 0
+    #validationString = ""
+    #while (i < inputLength):
+        #validationString += uInput[i]
+        #if (d.get(validationString) != None):
+            #counter = counter - (len(validationString) - 1)
+            #validationString = ""
+            #limitCounter = limitCounter + 1
+        #else:
+            #counter = counter + 1
+        #i = i + 1
+    #return counter == 0 and limitCounter < 4
+
+#Method to recursively validate an input
+def recValidateInput(uInput, d, i, counter, limitCounter, validationString):
+    if i == len(uInput):
+        return counter == 0 and limitCounter < 4
+    else:
         validationString += uInput[i]
         if (d.get(validationString) != None):
-            counter = counter - (len(validationString) - 1)
-            validationString = ""
-            limitCounter = limitCounter + 1
+            return recValidateInput(uInput, d, i + 1, counter - (len(validationString) - 1), limitCounter + 1, "")
         else:
-            counter = counter + 1
-        i = i + 1
-    return counter == 0 and limitCounter < 4
+            return recValidateInput(uInput, d, i + 1, counter + 1, limitCounter, validationString)
+            
+    
+    
+
 # Method that Hashes userInput via SHA256
 def hashInput_256(uInput):
     hashed_string = hashlib.sha256(uInput.encode('utf-8')).hexdigest()
@@ -82,61 +134,126 @@ def hashInput_512(uInput):
 # Method to crack the SHA256 encryption, 
 # uses for loops, to iterate from all combinations of words possible
 # will get all combination of "aaa..." possible before reaching "abc"
-def passwordCrack_256(result, userInput):
-    start = time()
-    user256 = hashInput_256(userInput)
-    for i in result:
-        holder = i
-        check256 = hashInput_256(holder)
-        if (check256 == user256):
-            return time() - start
-        for i in result: 
-            holder2 = holder + i
-            check256 = hashInput_256(holder2)
-            if (check256 == user256):
-                return time() - start
-            for i in result:
-                holder3 = holder2 + i 
-                check256 = hashInput_256(holder3)
-                if (check256 == user256):
-                    return time() - start
-                else:
-                    holder3 = ""
+#def passwordCrack_256(result, userInput):
+    #start = time()
+    #user256 = hashInput_256(userInput)
+    #for i in result:
+        #holder = i
+        #check256 = hashInput_256(holder)
+        #if (check256 == user256):
+            #return time() - start
+        #for i in result: 
+            #holder2 = holder + i
+            #check256 = hashInput_256(holder2)
+            #if (check256 == user256):
+                #return time() - start
+            #for i in result:
+                #holder3 = holder2 + i 
+                #check256 = hashInput_256(holder3)
+                #if (check256 == user256):
+                    #return time() - start
+                #else:
+                    #holder3 = ""
     
 # Method to crack the SHA512 encryption, 
 # uses for loops, to iterate from all combinations of words possible
 # will get all combination of "aaa..." possible before reaching "abc"        
-def passwordCrack_512(result, userInput):
-    start = time()
-    user512 = hashInput_512(userInput)
-    for i in result:
-        holder = i
-        check512 = hashInput_512(holder)
-        if (check512 == user512):
-            return time() - start
-        for i in result: 
-            holder2 = holder + i
-            check512 = hashInput_512(holder2)
-            if (check512 == user512):
-                return time() - start
-            for i in result:
-                holder3 = holder2 + i 
-                check512 = hashInput_512(holder3)
-                if (check512 == user512):
-                    return time() - start
-                else:
-                    holder3 = ""
+#def passwordCrack_512(result, userInput):
+    #start = time()
+    #user512 = hashInput_512(userInput)
+    #for i in result:
+        #holder = i
+        #check512 = hashInput_512(holder)
+        #if (check512 == user512):
+            #return time() - start
+        #for i in result: 
+            #holder2 = holder + i
+            #check512 = hashInput_512(holder2)
+            #if (check512 == user512):
+                #return time() - start
+            #for i in result:
+                #holder3 = holder2 + i 
+                #check512 = hashInput_512(holder3)
+                #if (check512 == user512):
+                    #return time() - start
+                #else:
+                    #holder3 = ""
         
+
+def receiveMaxCombinations(wordList, k):
+    i = 1
+    maxCount = 0
+    while i <= k:
+        maxCount =+ len(wordList)**i
+        i = i + 1
+    return maxCount
+
+        
+#QUEUE OF LENGTH NEEDED WITH '' empty spots
+def recCombos512(listOfWords, queue, i, k, limit, counter, userInput, extra):
+    wordInQueue = queue.popleft()
+    print("Word:",wordInQueue)
+    currentGuess = str(wordInQueue) + listOfWords[i]
+    shaGUESS = hashInput_512(currentGuess)
+    shaInput = hashInput_512(userInput)
+    print("STR:", currentGuess)
+    j = 0
+    while j < len(listOfWords):
+        queue.append(currentGuess)
+        j = j + 1
+    counter = counter + 1
+    print("Counter:",counter)
+    if (shaGUESS == shaInput):
+        print("FOUND")
+        return counter
+    elif (counter == receiveMaxCombinations(listOfWords, k) + extra):
+        return -1
+    elif (counter == receiveMaxCombinations(listOfWords, limit + 1)):
+        return recCombos512(listOfWords, queue, 0, k, limit + 1, counter, userInput, extra)
+    elif (i == len(listOfWords) - 1):
+        return recCombos512(listOfWords, queue, 0, k, limit, counter, userInput, extra)
+    else:
+        return recCombos512(listOfWords, queue, i + 1, k, limit, counter, userInput, extra)
+    
+def recCombos256(listOfWords, queue, i, k, limit, counter, userInput, extra):
+    wordInQueue = queue.popleft()
+    print("Word:",wordInQueue)
+    currentGuess = str(wordInQueue) + listOfWords[i]
+    shaGUESS = hashInput_256(currentGuess)
+    shaInput = hashInput_256(userInput)
+    print("STR:", currentGuess)
+    j = 0
+    while j < len(listOfWords):
+        queue.append(currentGuess)
+        j = j + 1
+    counter = counter + 1
+    print("Counter:",counter)
+    if (shaGUESS == shaInput):
+        print("FOUND!")
+        return counter
+    elif (counter == receiveMaxCombinations(listOfWords, k) + extra):
+        return -1
+    elif (counter == receiveMaxCombinations(listOfWords, limit + 1)):
+        return recCombos256(listOfWords, queue, 0, k, limit + 1, counter, userInput, extra)
+    elif (i == len(listOfWords) - 1):
+        return recCombos256(listOfWords, queue, 0, k, limit, counter, userInput, extra)
+    else:
+        return recCombos256(listOfWords, queue, i + 1, k, limit, counter, userInput, extra)
+
 # Prompts user to keep entering passwords to crack,
 # until the user prompts the letter q
+
 selectedString = ""
 while (selectedString != 'q'):
     selectedString = requestInput()
+    
 
-# Dictionary Swap
-result_swap = {v: k for k, v in result.items()}
+
+print(inputList)
 print(timeArray256)
 print(timeArray512)
+print(numberOfGuess)
+
 
 
 
