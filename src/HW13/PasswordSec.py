a92dbd865316b8f8fb6833a2e76d70ef61e5cc70
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-12-02 20:46:07 -0500

    FINALIZED

--- src/HW13/PasswordSec.py
+++ src/HW13/PasswordSec.py
@@ -9,11 +9,6 @@ from collections import deque
 import sys
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
-import numpy as np
-
-
-
-
 
 sys.setrecursionlimit(5000)
 
@@ -28,7 +23,6 @@ numberOfGuess = []
 # While loop to create a string of all words within
 # the read text file
 while i < len(contents):
-    
     concat = ""
     concat += str(i+1)
     concat += ": "
@@ -52,6 +46,8 @@ result_swap = {v: k for k, v in result.items()}
 timeArray256 = []
 timeArray512 = []
 
+#Method for specific math calculation for
+#combinations within recCombos
 def recExtras(l, theSum):
     if (l == 3):
         return theSum + 12
@@ -95,23 +91,7 @@ def requestInput():
     else:
         print("Invalid Input!")
     return userInput
-# Method to ensure a user input is valid 
-#def validateInput(uInput, d):
-    #inputLength = len(uInput)
-    #i = 0
-    #counter = 0
-    #limitCounter = 0
-    #validationString = ""
-    #while (i < inputLength):
-        #validationString += uInput[i]
-        #if (d.get(validationString) != None):
-            #counter = counter - (len(validationString) - 1)
-            #validationString = ""
-            #limitCounter = limitCounter + 1
-        #else:
-            #counter = counter + 1
-        #i = i + 1
-    #return counter == 0 and limitCounter < 4
+
 
 #Method to recursively validate an input
 def recValidateInput(uInput, d, i, counter, limitCounter, validationString):
@@ -135,55 +115,7 @@ def hashInput_256(uInput):
 def hashInput_512(uInput):
     hashed_string = hashlib.sha512(uInput.encode('utf-8')).hexdigest()
     return hashed_string
-# Method to crack the SHA256 encryption, 
-# uses for loops, to iterate from all combinations of words possible
-# will get all combination of "aaa..." possible before reaching "abc"
-#def passwordCrack_256(result, userInput):
-    #start = time()
-    #user256 = hashInput_256(userInput)
-    #for i in result:
-        #holder = i
-        #check256 = hashInput_256(holder)
-        #if (check256 == user256):
-            #return time() - start
-        #for i in result: 
-            #holder2 = holder + i
-            #check256 = hashInput_256(holder2)
-            #if (check256 == user256):
-                #return time() - start
-            #for i in result:
-                #holder3 = holder2 + i 
-                #check256 = hashInput_256(holder3)
-                #if (check256 == user256):
-                    #return time() - start
-                #else:
-                    #holder3 = ""
-    
-# Method to crack the SHA512 encryption, 
-# uses for loops, to iterate from all combinations of words possible
-# will get all combination of "aaa..." possible before reaching "abc"        
-#def passwordCrack_512(result, userInput):
-    #start = time()
-    #user512 = hashInput_512(userInput)
-    #for i in result:
-        #holder = i
-        #check512 = hashInput_512(holder)
-        #if (check512 == user512):
-            #return time() - start
-        #for i in result: 
-            #holder2 = holder + i
-            #check512 = hashInput_512(holder2)
-            #if (check512 == user512):
-                #return time() - start
-            #for i in result:
-                #holder3 = holder2 + i 
-                #check512 = hashInput_512(holder3)
-                #if (check512 == user512):
-                    #return time() - start
-                #else:
-                    #holder3 = ""
-        
-
+#Max combinations of any length list
 def receiveMaxCombinations(wordList, k):
     i = 1
     maxCount = 0
@@ -193,7 +125,7 @@ def receiveMaxCombinations(wordList, k):
     return maxCount
 
         
-#QUEUE OF LENGTH NEEDED WITH '' empty spots
+#Recursive method to use crack password given by user
 def recCombos512(listOfWords, queue, i, k, limit, counter, userInput, extra):
     wordInQueue = queue.popleft()
     print("Word:",wordInQueue)
@@ -218,7 +150,8 @@ def recCombos512(listOfWords, queue, i, k, limit, counter, userInput, extra):
         return recCombos512(listOfWords, queue, 0, k, limit, counter, userInput, extra)
     else:
         return recCombos512(listOfWords, queue, i + 1, k, limit, counter, userInput, extra)
-    
+
+#Recursive method to use crack password given by user
 def recCombos256(listOfWords, queue, i, k, limit, counter, userInput, extra):
     wordInQueue = queue.popleft()
     print("Word:",wordInQueue)
@@ -244,34 +177,13 @@ def recCombos256(listOfWords, queue, i, k, limit, counter, userInput, extra):
     else:
         return recCombos256(listOfWords, queue, i + 1, k, limit, counter, userInput, extra)
 
+
 # Prompts user to keep entering passwords to crack,
 # until the user prompts the letter q
 
 selectedString = ""
 while (selectedString != 'q'):
     selectedString = requestInput()
-    
-
-
-print(inputList)
-print(timeArray256)
-print(timeArray512)
-print(numberOfGuess)
-
-#plt.plot(timeArray256, numberOfGuess)
-#index = 0
-#while index < len(timeArray256):
-    #plt.plot(timeArray256[index], numberOfGuess[index])
-    #plt.annotate(inputList[index], xy=(timeArray256[index], numberOfGuess[index]), xytext=(timeArray256[index], numberOfGuess[index] + 50*index),
-            #arrowprops=dict(facecolor='black', shrink=0.01))
-    #index = index + 1
-    
-#plt.ylabel("Number of Searches")
-#plt.xlabel("Time for Word Guess")
-#plt.suptitle("SHA256:")
-#plt.show()
-
-#switch = input("")
 
 
 fig, ax = plt.subplots()
@@ -282,58 +194,57 @@ y = numberOfGuess
 l, = plt.plot(x, y, lw=2)
 ax.title.set_text('SHA 256')
 index = 0
-#while index < len(timeArray256):
-    #plt.plot(timeArray256[index], numberOfGuess[index])
-    #plt.annotate(inputList[index], xy=(timeArray256[index], numberOfGuess[index]), xytext=(timeArray256[index], numberOfGuess[index] + 50*index),
-            #arrowprops=dict(facecolor='black', shrink=0.01))
-    #index = index + 1
-    
 
-
+# Class creation in order to implement Swapping   
 class Index(object):
     ind = 0
-    global funcs # used so yu can access local list, funcs, here
-    def next(self, event):
+    global funcs 
+    def SHA512(self, event):
         self.ind += 1 
         i = self.ind %(len(funcs))
-        x,y,name = funcs[i]() # unpack tuple data
-        l.set_xdata(x) #set x value data
-        l.set_ydata(y) #set y value data
-        ax.title.set_text(name) # set title of graph
+        x, y, name = funcs[i]() 
+        l.set_xdata(x) 
+        l.set_ydata(y) 
+        ax.title.set_text(name)
+        ax.set(xlabel = "Seconds", ylabel = "Combos Attempted")
+        numOfItems = 0
+        while numOfItems < len(timeArray256):
+            ax.annotate(inputList[numOfItems], xy=(timeArray512[numOfItems], numberOfGuess[numOfItems]), xytext=(timeArray512[numOfItems], numberOfGuess[numOfItems] - numOfItems*100),
+                        arrowprops=dict(facecolor='black', shrink=0.05),)
+            numOfItems = numOfItems + 1
         plt.draw()
-
-    def prev(self, event):
-        self.ind -= 1 
-        i  = self.ind %(len(funcs))
-        x,y, name = funcs[i]() #unpack tuple data
-        l.set_xdata(x) #set x value data
-        l.set_ydata(y) #set y value data
-        ax.title.set_text(name) #set title of graph
+    def SHA256(self, event):
+        self.ind += 1 
+        i = self.ind %(len(funcs))
+        x, y, name = funcs[i]() 
+        l.set_xdata(x) 
+        l.set_ydata(y) 
+        ax.title.set_text(name)
+        ax.set(xlabel = "Seconds", ylabel = "Combos Attempted")
+        numOfItems = 0
+        while numOfItems < len(timeArray256):
+            ax.annotate(inputList[numOfItems], xy=(timeArray256[numOfItems], numberOfGuess[numOfItems]), xytext=(timeArray256[numOfItems], numberOfGuess[numOfItems] - numOfItems*100),
+                        arrowprops=dict(facecolor='black', shrink=0.05),)
+            numOfItems = numOfItems + 1
         plt.draw()
-
-def plot1():
+#PLOT for 256 SHA
+def SHA256PLOT():
     x = timeArray256
     y = numberOfGuess
-    return (x,y,"SHA256")
-
-def plot2():
+    return (x,y, "SHA256")
+#Plot for 512 SHA
+def SHA512PLOT():
     x = timeArray512
     y = numberOfGuess
     return (x,y,"SHA512")
-
-
-funcs = [plot1, plot2] # functions in a list so you can interate over
+#SWAP functionality below
+funcs = [SHA256PLOT, SHA512PLOT]
 callback = Index()
-axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
-axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-bnext = Button(axnext, 'Next')
-bnext.on_clicked(callback.next)
-bprev = Button(axprev, 'Previous')
-bprev.on_clicked(callback.prev)
+axSHA512 = plt.axes([0.20, 0.04, 0.20, 0.04])
+axSHA256 = plt.axes([0.60, 0.04, 0.20, 0.04])
+bSHA256 = Button(axSHA256, 'SHA256')
+bSHA256.on_clicked(callback.SHA256)
+bSHA512 = Button(axSHA512, 'SHA512')
+bSHA512.on_clicked(callback.SHA512)
 index = 0
 plt.show()
-
-
-
-
-
